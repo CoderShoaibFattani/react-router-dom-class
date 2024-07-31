e7c9e24c447fe1ba3fb74542d8e34e6d3c01be7b
Muhammad Shoaib Mushtaq <shoaibfattani2010@gmail.com>
2024-07-31 21:33:03 +0500

    Add files via upload

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import NavBar from './NavBar'
-import { Outlet } from 'react-router-dom'
+// import NavBar from './NavBar'
+// import { Outlet } from 'react-router-dom'
 
 const Home = () => {
   return (
